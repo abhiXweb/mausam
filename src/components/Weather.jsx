a55319fb52fd183d994b4e654184b4753abfe968
abhiXweb <dev.abhisharma@gmail.com>
2022-08-17 22:42:00 +0530

    updated weather app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,26 @@
 const Weather = () => {
-    
+    return (
+        <section className="weather">
+            <div className="container">
+                <div className="row">
+                    <div className="hero-img">
+                        <h2>Weather</h2>
+                    </div>
+                    <div className="weathers">
+                        <ul>
+                            <li>Mon</li>
+                            <li>Tue</li>
+                            <li>Wed</li>
+                            <li>Thu</li>
+                            <li>Fri</li>
+                            <li>Sat</li>
+                            <li>Sun</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Weather
